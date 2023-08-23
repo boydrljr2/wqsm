043782948af3f45dd13467cabe7b1ee0687e2e56
Robert Boyd <boydrljr@gmail.com>
2023-08-23 16:38:00 -0700

    Initial Commit

---
+++ wqsm/wqsmApp.swift
@@ -0,0 +1,17 @@
+//
+//  wqsmApp.swift
+//  wqsm
+//
+//  Created by bobsDevMac on 8/23/23.
+//
+
+import SwiftUI
+
+@main
+struct wqsmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
